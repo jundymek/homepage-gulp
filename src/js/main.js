dd37d0cb02646220f77ec6485e93403d9bdef8a8
jundymek <jundymek@gmail.com>
2019-07-08 23:45:37 +0200

    feat: moved homepage files to src

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,43 @@
 "use strict";
 
-import moment from 'moment';
+function openMenu() {
+    console.log("opem");
+    const menu = document.querySelector(".nav__list--js");
+    menu.classList.toggle("nav__list--open");
+    const header = document.querySelector(".header--js");
+    header.style.opacity = 1;
+}
 
-console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+function scroll() {
+    const header = document.querySelector(".header--js");
+    const menu = document.querySelector(".nav__list--js");
+    const arrow = document.querySelector(".arrow-up--js")
+    window.onscroll = () => {
+        menu ? menu.classList.remove("nav__list--js") : "";
+    };
+    if (scrollY > 0) {
+        header.style.opacity = 0.5;
+        arrow.style.display = 'block'
+    } else {
+        header.style.opacity = 1;
+        arrow.style.display = 'none'
+    }
+}
+
+function changer() {
+    const el = document.querySelector(".tofill");
+    if (el.innerHTML.length) {
+        el.innerHTML = "";
+    } else {
+        el.innerHTML = "Pojawiam się i znikam";
+    }
+}
+
+const hamburger = document.querySelector(".header__hamburger--js");
+const change = document.querySelector(".changer");
+hamburger.addEventListener("click", openMenu);
+window.addEventListener("scroll", scroll);
+if (change) {
+    change.addEventListener("click", changer);
+}
+
