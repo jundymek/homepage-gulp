720c010bbd4e8548be280e9215d0c44feb3f746a
jundymek <jundymek@gmail.com>
2019-07-28 12:13:12 +0200

    feat: hamburger menu x animation

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,14 @@
 
 function openMenu() {
     console.log("opem");
+    const hamburger = document.querySelector(".fas--js");
     const menu = document.querySelector(".nav__list--js");
+    const hamburgerClose = document.querySelector(".header__hamburger--close-js")
+    hamburgerClose.classList.toggle("header__hamburger--close")
     menu.classList.toggle("nav__list--open");
+    hamburger.classList.toggle("fab--animate")
+    hamburger.classList.toggle("fas")
+    hamburger.classList.toggle("fa-bars")
     const header = document.querySelector(".header--js");
     header.style.opacity = .8;
 }
@@ -18,8 +24,10 @@ function changer() {
 }
 
 const hamburger = document.querySelector(".header__hamburger--js");
+const hamburgerClose = document.querySelector(".header__hamburger--close-js")
 const change = document.querySelector(".changer");
 hamburger.addEventListener("click", openMenu);
+hamburgerClose.addEventListener("click",openMenu);
 window.addEventListener("scroll", scroll);
 if (change) {
     change.addEventListener("click", changer);
